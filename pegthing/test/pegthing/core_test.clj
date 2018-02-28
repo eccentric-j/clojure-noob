b3d1827c0ba7a40670e8bb9c776502a00f9d403d
Jay Zawrotny <jayzawrotny@gmail.com>
2018-02-28 00:54:27 -0500

    Added more pegthing tests

--- pegthing/test/pegthing/core_test.clj
+++ pegthing/test/pegthing/core_test.clj
@@ -31,3 +31,56 @@
   (testing "Connects two items in a board"
     (is (= (connect {} 10 1 3 6)
            {1 {:connections {6 3}}, 6 {:connections {1 3}}}))))
+
+(deftest connect-right-test
+  (testing "Connects pegs to the right"
+    (is (= (connect-right {} 6 3))
+        {1 {:connections {6 3}} 6 {:connections {1 3}}})))
+
+(deftest connect-down-left-test
+  (testing "Connects pegs to the down left"
+    (is (= (connect-down-left {} 8 3)
+           {3 {:connections {8 5}} 8 {:connections {3 5}}}))))
+
+(deftest connect-down-right-test
+  (testing "Connects pegs down right"
+    (is (= (connect-down-right {} 15 6)
+           {6 {:connections {15 10}} 15 {:connections {6 10}}}))))
+
+(deftest add-pos-test
+  (testing "Updates the board with new peg"
+    (is (= (add-pos {} 8 3)
+           {3 {:pegged true, :connections {8 5}}, 8 {:connections {3 5}}}))))
+
+(deftest new-board-test
+  (testing "Creates a new board map with set number of rows"
+    (is (= (new-board 2)
+           {:rows 2, 1 {:pegged true}, 2 {:pegged true}, 3 {:pegged true}}))))
+
+(deftest pegged-test
+  (testing "Returns true when target peg is pegged"
+    (is (= (pegged? {3 {:pegged true, :connections {8 5}}, 8 {:connections {3 5}}} 3)
+           true)))
+  (testing "Returns false when target peg is not pegged"
+    (is (= (pegged? {3 {:pegged false, :connections {8 5}}, 8 {:connections {3 5}}} 3)
+           false))))
+
+(deftest remove-peg-test
+  (testing "Returns board with :pegged set to false"
+    (is (= (remove-peg {3 {:pegged true, :connections {8 5}}, 8 {:connections {3 5}}} 3)
+           {3 {:pegged false, :connections {8 5}}, 8 {:connections {3 5}}}))))
+
+(deftest place-peg-test
+  (testing "Returns board with target peg :pegged set to true"
+    (is (= (place-peg {3 {:pegged false, :connections {8 5}}, 8 {:connections {3 5}}} 3)
+           {3 {:pegged true, :connections {8 5}}, 8 {:connections {3 5}}}))))
+
+(deftest move-peg-test
+  (testing "Returns board with pegged true on target and false on source"
+    (is (= (move-peg {3 {:pegged true, :connections {8 5}}, 8 {:pegged false, :connections {3 5}}} 3 8)
+           {3 {:pegged false, :connections {8 5}}, 8 {:pegged true :connections {3 5}}}))))
+
+(deftest valid-moves-test
+  (testing "Returns map of valid moves for a given pos"
+    (is (= (valid-moves (remove-peg (new-board 5) 12) 5)
+           {12 8}))))
